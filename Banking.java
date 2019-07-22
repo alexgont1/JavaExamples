5e8d8819b9c0922622619acdf36581f33eec1b7b
alexgont1 <alexgont1@gmail.com>
2019-07-22 13:35:28 -0400

    Simple example - how to open web page with using Firefox geckodriver-exe(paste exe in Java Project)

---
+++ Banking.java
@@ -0,0 +1,19 @@
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Banking {
+	
+	public void salary() {
+		Calculator calc = new Calculator();
+		calc.add();
+	}
+	
+
+	public static void main(String[] args) {
+		
+	/*	FirefoxDriver driver = new FirefoxDriver();
+		driver.get("http://way2automation.com");*/
+		
+		System.out.println("Way" + " to go.");
+	}
+
+}
