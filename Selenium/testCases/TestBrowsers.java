4d5c4f476f101b395112c6bff4b9b4d644d46868
alexgont1 <alexgont1@gmail.com>
2019-07-26 10:23:48 -0400

    GoogleChrome configuration for Selenium testing

--- Selenium/testCases/TestBrowsers.java
+++ Selenium/testCases/TestBrowsers.java
@@ -1,5 +1,6 @@
 package testCases;
 
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class TestBrowsers {
@@ -9,10 +10,11 @@ public class TestBrowsers {
 		//To work with Firefox you must put in project 
 		//geckodriver (copy-paste exe file in project)
 		//OR BETTER TO SET SYSTEM PROPERTY:
-		System.setProperty("webdriver.gecko.driver", "S:\\Selenium\\geckodriver.exe");
+		//System.setProperty("webdriver.gecko.driver", "S:\\Selenium\\geckodriver.exe");
+		System.setProperty("webdriver.chrome.driver", "S:\\Selenium\\chromedriver.exe");
 		
-		
-		FirefoxDriver driver = new FirefoxDriver();
+		//FirefoxDriver driver = new FirefoxDriver();
+		ChromeDriver driver = new ChromeDriver();
 		//Auto import libraries: CTRL+SHIFT+o
 		
 
