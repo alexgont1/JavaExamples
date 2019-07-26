fcc6484fc0dbd05c45edd54ea93dcea607d0cac4
alexgont1 <alexgont1@gmail.com>
2019-07-26 15:02:09 -0400

    Possible solution to work with IE webDriver

--- Selenium/testCases/TestBrowsers.java
+++ Selenium/testCases/TestBrowsers.java
@@ -1,24 +1,43 @@
 package testCases;
 
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.ie.InternetExplorerDriver;
 
 public class TestBrowsers {
+	
+	
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		
 		//To work with Firefox you must put in project 
 		//geckodriver (copy-paste exe file in project)
 		//OR BETTER TO SET SYSTEM PROPERTY:
 		//System.setProperty("webdriver.gecko.driver", "S:\\Selenium\\geckodriver.exe");
-		System.setProperty("webdriver.chrome.driver", "S:\\Selenium\\chromedriver.exe");
+		//System.setProperty("webdriver.chrome.driver", "S:\\Selenium\\chromedriver.exe");
+		System.setProperty("webdriver.ie.driver", "S:\\Selenium\\IEDriverServerV3-6-32x.exe");
+		
+		//DesiredCapabilities capabilities = DesiredCapabilities.internetExplorer();
+        // this line of code is to resolve protected mode issue 
+		//capabilities.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
+        //capabilities.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
 		
 		//FirefoxDriver driver = new FirefoxDriver();
-		ChromeDriver driver = new ChromeDriver();
-		//Auto import libraries: CTRL+SHIFT+o
+		//ChromeDriver driver = new ChromeDriver();
 		
-
-		driver.get("http://way2automation.com");
+		//Auto import libraries: CTRL+SHIFT+o
+		//options.ignoreZoomSettings();
+		InternetExplorerDriver driver = new InternetExplorerDriver();
+		
+		//driver.wait(3000);		
+		
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		
+		
+		driver.get("https://www.google.com");
+		
+		driver.close();
 		
 		
 	
