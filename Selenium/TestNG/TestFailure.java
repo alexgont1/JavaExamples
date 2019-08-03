db725eb3054261ba54f8e7d26da87cb08760e2df
alexgont1 <alexgont1@gmail.com>
2019-08-03 09:15:11 -0400

    Failure in test and skipping tests with exception message

---
+++ Selenium/TestNG/TestFailure.java
@@ -0,0 +1,14 @@
+package testCases;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestFailure {
+	
+	@Test
+	public void doLogin() {
+		Assert.fail("Failing the login test");
+		System.out.println("Capture screenshot");
+	}
+
+}
