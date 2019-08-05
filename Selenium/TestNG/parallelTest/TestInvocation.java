a1b72312adf7917f7a476a61fbe89597b907eeba
alexgont1 <alexgont1@gmail.com>
2019-08-05 09:05:42 -0400

    TestNG - parallel test execution (Java_BaseTest + TestInvocation + pom update)

---
+++ Selenium/TestNG/parallelTest/TestInvocation.java
@@ -0,0 +1,18 @@
+package parallelTest;
+
+import org.testng.annotations.Test;
+
+public class TestInvocation extends BaseTest{
+	
+	@Test(invocationCount=5,threadPoolSize=5)//times,parallel
+	public void executeTest() throws InterruptedException {
+		
+		driver = getDriver("chrome");
+		driver.get("http://google.com");
+		System.out.println(driver.getTitle());
+		
+		driver.quit();
+		
+	}
+
+}
