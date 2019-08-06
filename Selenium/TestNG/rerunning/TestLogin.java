b1f602ff06d86f7d5c24a11cf1ec08a9c55fe6ab
alexgont1 <alexgont1@gmail.com>
2019-08-06 12:20:50 -0400

    TestNG - Rerunning failed tests

---
+++ Selenium/TestNG/rerunning/TestLogin.java
@@ -0,0 +1,20 @@
+package rerunning;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestLogin {
+
+	@Test(priority=1)
+	public void doLogin() {
+		System.out.println("Executing Login Test");
+	}
+	
+	static int count=0;
+	@Test(priority=2)
+	public void registerUsers() {
+		
+		System.out.println(count++);
+		Assert.fail("Fail while Registering a user");
+	}
+}
