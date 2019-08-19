f75ce21d72226c951e397a223584f46ef21c9853
alexgont1 <alexgont1@gmail.com>
2019-08-19 09:54:21 -0400

    Select 1 element from DB query

--- JavaDB/TestDBqueries.java
+++ JavaDB/TestDBqueries.java
@@ -4,14 +4,19 @@ import java.util.List;
 public class TestDBqueries {
 
 	public static void main(String[] args) throws SQLException {
-		
+
 		DBManager.setMysqlDBConnection();
 
-		//1 column
+		// 1 column
 		List<String> query = DBManager.getMysqlQuery("select City from students;");
-		
-		for(String q: query) {
+
+		for (String q : query) {
 			System.out.println(q);
 		}
+
+		// or you can select 1 element
+		String s = DBManager
+				.getMysqlQuery("select City from students;").get(0);
+		System.out.println(s);
 	}
 }
